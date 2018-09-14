59e0c9c3f8941f7a650acc96f92f9cc6ac061f43
James <tytechortz@gmail.com>
2018-09-13 18:23:32 -0600

    Commit 1-Verbal Questions

---
+++ app.js
@@ -0,0 +1,7 @@
+//1.Parameter-a parameter is a temporary variable name to which an argument can be assigned.
+//  Argument-The input to the parameter
+
+//2. Return gives a value to a function. Console.log simply allows us to see the results.
+//3. It allows the funciton to be reused, for DRY code.
+
+
