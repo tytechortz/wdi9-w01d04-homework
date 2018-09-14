04974688da7556767e7986c03d59575d06566752
James <tytechortz@gmail.com>
2018-09-14 06:45:27 -0600

    fixes palindrome problem to deal with capital letters.

--- app.js
+++ app.js
@@ -3,38 +3,42 @@
 
 //2. Return gives a value to a function. Console.log simply allows us to see the results.
 //3. It allows the funciton to be reused, for DRY code.
-/*
+
 const checkPalindrome = (word) => {
-   const reversedWord = word.split('').reverse().join('');
-    if (word === reversedWord) {
+    //Changes word to all lowercase;
+    const caseWord = word.toLowerCase();
+    const reversedWord = caseWord.split('').reverse().join('');
+    if (reversedWord === caseWord) {
     console.log('true');
-    } else {
+        } else {
     console.log('false');
     } 
 }
+
 checkPalindrome('Radar');
 checkPalindrome('Borscht');
-*/
+
 
 
 //3. Digit Sum
-
+/*
 
 //4. Pythagoras
-/*
+
 const calculateSide = (sideA, sideB) => {
     const sideC = Math.sqrt(Math.pow(sideA,2) + Math.pow(sideB,2));
    console.log(sideC);
 }
 calculateSide(8, 6);
-*/
+
 
 //5. Sum Array
-/*
+
 console.log([1, 2, 3, 4, 5, 6].reduce((a, b) => a + b, 0));
-*/
+
 
 //6.
+
 const checkPrime = (num) => {
     for(let i = 2; i <= Math.sqrt(i); i++)
         if (num % i === 0) 
@@ -42,4 +46,4 @@ const checkPrime = (num) => {
         return num !== 1;  
     }
 checkPrime(9);
-
+*/
